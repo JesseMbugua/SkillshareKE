eaa3b8c353fe41c6794a8a417d82b90e437222c2
justpmn <peter.nyamu@strathmore.edu>
2025-11-02 15:54:49 +0300

    reintroduced firebase stuff

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,18 +1,18 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    id("org.jetbrains.kotlin.plugin.compose") // ✅ add this line
+    alias(libs.plugins.google.gms.google.services)
+    id("org.jetbrains.kotlin.plugin.compose")
 }
-
 
 android {
     namespace = "com.example.skillshare"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.skillshare"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -28,10 +28,10 @@ android {
         }
     }
 
+    // Jetpack Compose settings
     buildFeatures {
         compose = true
     }
-
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.3"
     }
@@ -40,7 +40,6 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-
     kotlinOptions {
         jvmTarget = "11"
     }
@@ -52,6 +51,11 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
 
+    // --- Firebase Libraries ---
+    implementation(libs.firebase.firestore)
+    implementation(libs.firebase.auth)
+    implementation(libs.firebase.storage)
+
     // Jetpack Compose
     implementation("androidx.activity:activity-compose:1.9.2")
     implementation("androidx.compose.ui:ui:1.7.4")
@@ -61,10 +65,10 @@ dependencies {
     // Navigation Compose
     implementation("androidx.navigation:navigation-compose:2.8.3")
 
-    // Lifecycle
+    // Lifecycle Compose
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.5")
 
-    // Tooling and tests
+    // Tooling and Tests
     debugImplementation("androidx.compose.ui:ui-tooling:1.7.4")
     debugImplementation("androidx.compose.ui:ui-test-manifest:1.7.4")
     testImplementation("junit:junit:4.13.2")
